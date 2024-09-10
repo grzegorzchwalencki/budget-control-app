44e54227772b138cabe2b1ac81e432d951d424e3
grzegorzchwalencki <bukacieszuka1@gmail.com>
2024-09-10 22:12:06 +0200

    Initial commit

---
+++ src/test/java/com/MyApp/budgetControl/BudgetControlApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MyApp.budgetControl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetControlApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
