d11633389046c0b876c23cefd77c2cfd6f18a035
grzegorzchwalencki <bukacieszuka1@gmail.com>
2024-10-07 23:58:58 +0200

    Add ExpenseRequestDTO, ExpenseResponseDTO, Implement h2 database, JpaRepository interface, change id to UUID format, Date to Instant and update tests

--- src/main/java/com/MyApp/budgetControl/domain/expense/ExpenseJpaRepository.java
+++ src/main/java/com/MyApp/budgetControl/domain/expense/ExpenseJpaRepository.java
@@ -1,11 +1,13 @@
 package com.MyApp.budgetControl.domain.expense;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
+@Repository
 public interface ExpenseJpaRepository extends ExpenseRepository, JpaRepository<ExpenseEntity, UUID> {
     ExpenseEntity save(ExpenseEntity newExpense);
 
