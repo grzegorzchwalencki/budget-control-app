96e373f193701993233c81e01ac2af0258246f5f
grzegorzchwalencki <bukacieszuka1@gmail.com>
2025-04-25 23:37:08 +0200

    Refactor

--- src/test/functional/com/MyApp/budgetControl/api/TestcontainersConfiguration.java
+++ src/test/functional/com/MyApp/budgetControl/api/TestcontainersConfiguration.java
@@ -2,15 +2,18 @@ package com.MyApp.budgetControl.api;
 
 import org.springframework.boot.test.context.TestConfiguration;
 import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @TestConfiguration
+@Testcontainers
 class TestContainersConfiguration {
 
-  private static final PostgreSQLContainer postgreSQLContainer = new PostgreSQLContainer<>
+  @Container
+  static PostgreSQLContainer<?> postgreSQLContainer = new PostgreSQLContainer<>
       ("postgres:17.2-alpine3.21");
 
   static {
-    postgreSQLContainer.start();
     System.setProperty("spring.datasource.url", postgreSQLContainer.getJdbcUrl());
     System.setProperty("spring.datasource.password", postgreSQLContainer.getPassword());
     System.setProperty("spring.datasource.username", postgreSQLContainer.getUsername());
