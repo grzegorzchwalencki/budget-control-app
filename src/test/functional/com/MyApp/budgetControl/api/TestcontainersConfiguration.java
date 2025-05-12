8bd9af5b420db791ef4ed3618794b611b3d83cdd
grzegorzchwalencki <bukacieszuka1@gmail.com>
2025-05-12 23:37:32 +0200

    Change junit dependencies

--- src/test/functional/com/MyApp/budgetControl/api/TestcontainersConfiguration.java
+++ src/test/functional/com/MyApp/budgetControl/api/TestcontainersConfiguration.java
@@ -1,6 +1,5 @@
 package com.MyApp.budgetControl.api;
 
-import org.junit.jupiter.api.AfterAll;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
@@ -19,10 +18,5 @@ abstract class TestContainersConfiguration {
     registry.add("spring.datasource.url", postgres::getJdbcUrl);
     registry.add("spring.datasource.username", postgres::getUsername);
     registry.add("spring.datasource.password", postgres::getPassword);
-  }
-
-  @AfterAll
-  static void afterAll() {
-    postgres.stop();
   }
 }
