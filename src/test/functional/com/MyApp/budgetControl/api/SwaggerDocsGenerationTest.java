cabbb54c01981253c8f25f6d0aa28fffde434b28
grzegorzchwalencki <bukacieszuka1@gmail.com>
2025-07-12 23:08:22 +0200

    Changed test

--- src/test/functional/com/MyApp/budgetControl/api/SwaggerDocsGenerationTest.java
+++ src/test/functional/com/MyApp/budgetControl/api/SwaggerDocsGenerationTest.java
@@ -1,31 +1,27 @@
 package com.MyApp.budgetControl.api;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.test.web.server.LocalServerPort;
-import org.springframework.http.ResponseEntity;
+import org.springframework.test.web.servlet.MockMvc;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
+@AutoConfigureMockMvc
 class SwaggerDocsGenerationTest {
 
-  @LocalServerPort
-  private int port;
+    @Autowired
+    private MockMvc mockMvc;
 
-  @Autowired
-  private TestRestTemplate restTemplate;
-
-  @Test
-  void swaggerDocsAreGeneratedCorrectly() {
-    String url = "http://localhost:" + port + "/v3/api-docs";
-
-    ResponseEntity<String> response = restTemplate.getForEntity(url, String.class);
-
-    assertThat(response.getStatusCode().is2xxSuccessful()).isTrue();
-    assertThat(response.getBody()).isNotNull();
-    assertThat(response.getBody()).contains("\"openapi\":\"3.0.1\"");
-  }
+    @Test
+    void shouldExposeSwaggerDocs() throws Exception {
+      mockMvc.perform(get("/v3/api-docs"))
+          .andExpect(status().isOk())
+          .andExpect(content().string(containsString("\"OpenAPI definition\"")));
+    }
 }
